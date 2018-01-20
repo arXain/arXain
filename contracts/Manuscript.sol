8824e133bb899957837d3e0e9b8df55add5c4e08
Saul A. Kohn <saul.aryeh.kohn@gmail.com>
2018-01-20 00:29:36 -0500

    initial functionality for manuscript uploads

--- contracts/Manuscript.sol
+++ contracts/Manuscript.sol
@@ -2,25 +2,66 @@ pragma solidity ^0.4.11;
 
 import "./Mortal.sol";
 
-
 contract Manuscript{
-    string corpus;
-    Mortal m;
+    Mortal author;
+    string corpusID;
+    string doi;
+    uint8 revision;
+
+    enum reviewStatus {unreviewed, needsWork, accepted}
+    reviewStatus status;
 
     function Manuscript(){
-        corpus = "Give me Science!";
-        m = new Mortal();
+        author = new Mortal();
+        corpusID = '0x0';
+        doi = '0x0';
+        revision = 0;
+        status = reviewStatus.unreviewed;
     }
-    
-    function getCorpus() constant returns (string) {
-        return corpus;
+
+    function initialManuscript(string _corpusID, string _doi){
+        /*
+        TODO: PATCH TRUFFLE FUNCTIONALITY
+        if (msg.sender != getAddress()) return;
+        */
+        if (revision != 0) return;
+        setCorpusID(_corpusID);
+        setDoi(_doi);
     }
-    
-    function setCorpus(string _corpus){
-        corpus = _corpus;
+
+    function getStatus() constant returns (uint){
+        return uint(status);
     }
-    
+
+    function setStatus(reviewStatus _status){
+        status = _status;
+    }
+
+    function getRevision() constant returns (uint8){
+        return revision;
+    }
+
+    function getCorpusID() constant returns (string) {
+        return corpusID;
+    }
+
+    function setCorpusID(string _corpusID){
+        corpusID = _corpusID;
+    }
+
+    function getDoi() constant returns (string){
+        return doi;
+    }
+
+    function setDoi(string _doi){
+        doi = _doi;
+    }
+
     function getAddress() constant returns (address) {
-        return m.getOwner();
+        return author.getOwner();
+    }
+    //testing
+    function getSender() constant returns (address) {
+        return msg.sender;
     }
 }
--- test/manuscript.js
+++ test/manuscript.js
@@ -1,15 +1,4 @@
 var Manuscript = artifacts.require('./contracts/Manuscript.sol');
-contract('Manuscript:getCorpus', function(accounts) {
-  it("should return a correct string", function(done) {
-    var manuscript = Manuscript.deployed();
-    manuscript.then(function(contract){
-      return contract.getCorpus.call(); // **IMPORTANT
-    }).then(function(result){
-      assert.isTrue(result === 'Give me Science!');
-      done();
-    })
-  });
-});
 contract('Manuscript:getAddress', function(accounts) {
   it("should return an address", function(done) {
     var manuscript = Manuscript.deployed();
