d4c51792e0ea44c8feb8f7f48bdcc675773c06b8
Bijan Haney <bijan.haney@cern.ch>
2018-01-26 00:44:18 -0500

    shifting to vue framework

---
+++ src/components/App.vue
@@ -0,0 +1,18 @@
+<!-- App.vue (parent app) -->
+<template>
+	<div id="app">
+		<navbar-component></navbar-component>
+	</div>
+</template>
+
+<script>
+import NavbarComponent from '../components/NavbarComponent.vue'
+export default {
+			name: 'app',
+			components: {NavbarComponent}, //Register other components
+		}
+</script>
+
+<style scoped>
+</style>
+
---
+++ src/components/navbarComponent.vue
@@ -0,0 +1,46 @@
+<!-- Navbar Component -->
+<template>
+	<nav class="navbar navbar-inverse">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">ar&chi;ain</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+		  <form class="navbar-form navbar-right">
+			<div class="form-group">
+			  <a class="form-control" href="/submit.html">Submit a paper</a>
+			</div>
+			<div class="form-group">
+			  <a class="form-control" href="/ammend.html">Revise a paper</a>
+			</div>
+			<div class="form-group">
+			  <a class="form-control" href="/status.html">Check blockchain</a>
+			</div>
+			<div class="form-group">
+			  <a class="form-control" href="/comments.html">Comment on a paper</a>
+			</div>
+			<div class="form-group">
+			  <a class="form-control" href="/about.html">About Us</a>
+			</div>
+		  </form>
+		</div><!--/.navbar-collapse -->
+	</nav>
+</template>
+
+<script>
+    module.exports = {
+        data(){
+            return {}
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
