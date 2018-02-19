90010b04d1fad88c92cc91ab3141ba9b8366c6f8
Bijan Haney <bijan.haney@cern.ch>
2018-02-18 20:24:43 -0500

    trying to resolve dependencies

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,10 @@
 import Vue from 'vue'
-import jQuery from 'jquery'
-Vue.use(jQuery);
 
 import Web3Box from '../components/Web3Box.vue'
-import NavbarHome from '../components/NavbarHome.vue'
-Vue.component('navbar-home',NavbarHome);
 
-if($("#app").length != 0) {
-    new Vue({
-          el: '#app',
-          template: '<Web3Box/>',
-          components: { Web3Box }
-    })
-}
+new Vue({
+      el: '#app',
+      template: '<Web3Box/>',
+      components: { Web3Box }
+})
 
-if($("#home").length != 0) {
-    new Vue({
-          el: '#home',
-          template: '<navbar-home/>',
-          components: { NavbarHome }
-    })
-}
