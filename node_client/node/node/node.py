539a509a1fa92f0ea255c205e40ad704d581540b
Bijan Haney <bijan.haney@cern.ch>
2018-02-20 17:35:09 +0100

    removing urlparse

--- node_client/node/node/node.py
+++ node_client/node/node/node.py
@@ -3,7 +3,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash, jsonify
 from flask_cors import CORS, cross_origin
 from pyXain import pyXain
-from urlparse import urlparse
 
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
